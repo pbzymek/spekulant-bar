eb93019f46975f48f67c2313d04825aeb24289c7
Piotr Bzymek <piotr.bzymek@u2i.com>
2015-07-06 09:58:33 +0200

    Initial Commit

---
+++ SpekulantBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SpekulantBar
+//
+//  Created by Piotr Bzymek on 06/07/15.
+//  Copyright (c) 2015 XtremeRR. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
